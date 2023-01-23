f7ef336c948d39643681408079fcd7aee768a38f
Dmytro <dmytro.osiychuk@gmail.com>
2023-01-23 22:47:35 +0100

    Add model references

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -104,6 +104,12 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
+    ],
+    guides: [
+      { 
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
     ]
   },
   {
@@ -127,6 +133,11 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map( async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// })
 //the post middleware has access to the saved doc
 tourSchema.post('save', function (doc, next) {
   next();
@@ -137,6 +148,13 @@ tourSchema.post('save', function (doc, next) {
 //this stands for query object, this middleware will run for any query starting with find
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
+  next();
+});
+tourSchema.pre(/^find/, function (next) {
+  this.populate({ 
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
   next();
 });
 tourSchema.post(/^find/, function (docs) {});
